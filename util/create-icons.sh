786fc0c00ac3cfbbafa94767b2cca66428eb9367
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2025-09-27 16:23:56 +0900

    add disabled icon

--- util/create-icons.sh
+++ util/create-icons.sh
@@ -5,17 +5,18 @@ ROOT=$(dirname -- "$0")/..
 cd "$ROOT/images"
 
 create_logo() {
-    size=$1
-    filename=$2
+    original=$1
+    size=$2
+    filename=$3
 
-    convert -background none "$ORIGINAL" -resize "${size}x${size}" "$filename"
+    convert -background none "$original" -resize "${size}x${size}" "$filename"
     optipng "$filename"
 }
 
-ORIGINAL=icon.svg
-
 SIZES="16 32 48 128"
-for s in $SIZES
-do
-    create_logo "$s" "icon-${s}.png"
+for svgFile in *.svg; do
+    name="${svgFile%.svg}"
+    for s in $SIZES; do
+        create_logo "$svgFile" "$s" "${name}-${s}.png"
+    done
 done
