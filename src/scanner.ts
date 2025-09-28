fd9fcaa86a0bd9dcfe490a39f055519db362dab2
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2025-09-28 13:34:57 +0900

    add type to secretPatterns

--- src/scanner.ts
+++ src/scanner.ts
@@ -1,4 +1,5 @@
 export interface Secret {
+    type: string;
     pattern: string;
     match: string; // the found secret
 }
@@ -6,12 +7,13 @@ export interface Secret {
 export function scan(content: string): Secret[] {
     const results: Secret[] = [];
 
-    secretPatterns.forEach((pattern: RegExp) => {
-        const matches = content.match(pattern);
+    secretPatterns.forEach((secretPattern) => {
+        const matches = content.match(secretPattern.pattern);
         if (matches) {
             matches.forEach((match: string) => {
                 results.push({
-                    pattern: pattern.source,
+                    type: secretPattern.type,
+                    pattern: secretPattern.pattern.source,
                     match: match,
                 });
             });
@@ -21,9 +23,26 @@ export function scan(content: string): Secret[] {
     return results;
 }
 
-const secretPatterns: RegExp[] = [
-    /-----BEGIN .*PRIVATE KEY-----[\s\S]*?-----END .*PRIVATE KEY-----/g,
-    /sk_live_[a-zA-Z0-9]{24}/g, // Stripe secret key
-    /xoxb-[0-9]{11}-[0-9]{11}-[a-zA-Z0-9]{24}/g, // Slack bot token
-    /api[_-]?key\s*[:=]\s*['"][^'"]+['"]/gi,
+const secretPatterns = [
+    {
+        type: "privateKey",
+        pattern:
+            /-----BEGIN .*PRIVATE KEY-----[\s\S]*?-----END .*PRIVATE KEY-----/g,
+    },
+    {
+        type: "stripeAccessToken",
+        pattern: /(?:sk|rk)_(?:test|live|prod)_[a-zA-Z0-9]{24}/g,
+    },
+    {
+        type: "slackBotToken",
+        pattern: /xoxb-[0-9]{11}-[0-9]{11}-[a-zA-Z0-9]{24}/g,
+    },
+    {
+        type: "apiKey",
+        pattern: /api[_-]?key\s*[:=]\s*['"][^'"]+['"]/gi,
+    },
+    {
+        type: "password",
+        pattern: /passw(?:or)d\s*[:=]\s*['"][^'"]+['"]/gi,
+    },
 ];
