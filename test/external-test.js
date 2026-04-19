b93cb362a2ce4dfe983005044354442f1d16afb7
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2026-04-19 22:38:27 +0900

    add some more test secrets

--- test/external-test.js
+++ test/external-test.js
@@ -1,5 +1,8 @@
 // External JavaScript file with embedded secrets for testing
 
+// google API key
+const googleKey = "AIza11111111111111111111111111111111111";
+
 // RSA private key
 const rsaKey = `-----BEGIN RSA PRIVATE KEY-----
 MIIEowIBAAKCAQEAwK1YzUGjbCVqcI1LMGhSNZb0nZNRoZwqL5zO8fVNXp2JvOKX
@@ -17,13 +20,22 @@ qWJrHlZzNJMbQLJWJqgJzQgHzGQJzLJRFQ==
 // Slack bot token
 const slackToken = "xoxb-12345678901-12345678901-abcdefghijklmnopqrstuvwx";
 
+// Generic API key
+const apiKey = "a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0";
+
+// Password
+const password = "Sup3rS3cr3tP@ssw0rd!";
+
 console.log("External test secrets loaded");
 
 // Function that might be called dynamically
 function loadSecrets() {
     return {
-        rsa: rsaKey,
-        ec: ecKey,
-        slack: slackToken
+        googleKey,
+        rsaKey,
+        ecKey,
+        slackToken,
+        apiKey,
+        password,
     };
 }
