a262b319a8c402265f69a76abbd92acf6aadbbfe
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2026-07-19 09:33:56 +0900

    remove unused activeTab permission

--- src/util/browser.ts
+++ src/util/browser.ts
@@ -3,6 +3,7 @@ export async function getActiveTabId(): Promise<number> {
     const queryOptions = { active: true, currentWindow: true };
     // `tab` will either be a `tabs.Tab` instance or `undefined`.
     const [tab] = await chrome.tabs.query(queryOptions);
+    // Note: 'activeTab' permission only needed for title & url-related properties
     if (tab.id === undefined) {
         throw new Error("Couldn't get active tab");
     }
